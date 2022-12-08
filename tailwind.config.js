a834ffe73a19a200c2283b7a08fa2459813a9d5d
Hoang Cong Phan <hoangcp8700@gmail.com>
2022-12-08 10:08:18 +0700

    :fix: Bugfix: refactor css style

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./containers/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       spacing: {
@@ -24,20 +25,56 @@ module.exports = {
         lg: "1200px",
         xl: "1440px",
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.gray.700"),
+            "h1,h2": {
+              fontWeight: 700,
+              color: theme("colors.gray.900"),
+              letterSpacing: theme("letterSpacing.tight"),
+            },
+            h3: {
+              fontWeight: "600",
+              color: theme("colors.gray.900"),
+            },
+            "h4,h5,h6": {
+              color: theme("colors.gray.900"),
+            },
+          },
+        },
+        dark: {
+          css: {
+            color: theme("colors.gray.300"),
+            "h1,h2": {
+              fontWeight: 700,
+              color: theme("colors.gray.100"),
+              letterSpacing: theme("letterSpacing.tight"),
+            },
+            h3: {
+              fontWeight: "600",
+              color: theme("colors.gray.100"),
+            },
+            "h4,h5,h6": {
+              color: theme("colors.gray.100"),
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/line-clamp"), // use to truncate text to a fixed number of lines.
-    require("@tailwindcss/aspect-ratio"), // give an element a fixed aspect ratio.
+    // require("@tailwindcss/aspect-ratio"), // give an element a fixed aspect ratio.
     plugin(({ matchUtilities, addUtilities, addBase, theme, addVariant, e }) => {
       // registering new base styles
       addBase({
-        h1: { fontSize: theme("fontSize.3xl") },
-        h2: { fontSize: theme("fontSize.2xl") },
-        h3: { fontSize: theme("fontSize.xl") },
-        h4: { fontSize: theme("fontSize.lg") },
+        h1: { fontSize: theme("fontSize.3xl"), fontWeight: 700, color: theme("colors.gray.900") },
+        h2: { fontSize: theme("fontSize.2xl"), fontWeight: 700, color: theme("colors.gray.900") },
+        h3: { fontSize: theme("fontSize.xl"), fontWeight: 700, color: theme("colors.gray.900") },
+        h4: { fontSize: theme("fontSize.lg"), fontWeight: 700, color: theme("colors.gray.900") },
         p: { fontSize: theme("fontSize.base") },
         span: { fontSize: theme("fontSize.sm") },
       });
